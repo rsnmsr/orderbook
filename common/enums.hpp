6a752981f0163ae3ceebd0d04b2b5d54c188a823
Roshan Kumar <rkm191185@gmail.com>
2026-03-24 23:01:44 +0530

    Adding the order structure

---
+++ common/enums.hpp
@@ -0,0 +1,15 @@
+
+enum Side {
+    BUY = 1,
+    SELL = 2
+};
+
+enum EventType {
+    ADD = 1,
+    CANCEL = 2
+};
+
+enum OrderType{
+    LIMIT = 1,
+    MARKET = 2
+};
---
+++ orders/order.hpp
@@ -0,0 +1,35 @@
+#ifndef ORDER_HPP
+#define ORDER_HPP
+#include<string>
+#include<chrono>
+#include<enums.hpp>
+
+class Order{
+    protected:
+        std::string order_id;
+        std::chrono::system_clock::time_point timestamp;
+        int quantity;
+        std::string ticker;
+        Side side;
+
+    public:
+         Order (const std::string& order_id, 
+              const std::chrono::system_clock::time_point& timestamp, 
+              int quantity, 
+              const std::string& ticker, 
+              Side side
+            );
+
+        // We have various getters to obtain the order state 
+        const std::string& getOrderId();
+        int getQuantity();
+        const std::string& getTicker();
+        Side getSide();
+        const std::chrono::system_clock::time_point& getTimestamp();
+
+        virtual ~Order() = default;
+        virtual void execute() = 0;
+
+};
+
+#endif
